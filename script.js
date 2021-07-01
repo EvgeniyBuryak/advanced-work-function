222b020eb566224f8b010abf6b1df4a9fc608eeb
Jhonah <buryakone@gmail.com>
2021-07-01 17:24:20 +0700

    тестирую скорость работы алгоритма через микробенчмаркинг

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-function sumTo(num) {
+function sumToRecursion(num) {
     if (num < 1) {
         return 0;
     } else {
-        return num += sumTo(num - 1);
+        return num += sumToRecursion(num - 1);
     }
 }
 
+/*
 console.log(sumTo(1));  // = 1
 console.log(sumTo(2));  // = 2 + 1 = 3
 console.log(sumTo(3));  // = 3 + 2 + 1 = 6
 console.log(sumTo(4));  // = 4 + 3 + 2 + 1 = 10
 console.log(sumTo(100));// = 100 + 99 + ... + 2 + 1 = 5050
+*/
+
+function bench(f) {
+    let date1 = new Date(0);
+    let date2 = new Date();
+
+    let start = Date.now();
+    for (let i = 0; i < 100000; i++) f(100); // тестируем на 100
+    return Date.now() - start;
+}
+
+//let time1 = 0;
+let time2 = 0;
+
+// добавляем для "разогрева" перед основным циклом
+//bench(sumTo);
+bench(sumToRecursion);
+
+// а теперь тестируем производительность
+// bench поочерёдно запускаются 10 раз
+for (let i = 0; i < 10; i++) {
+    //time1 += bench(sumTo);
+    time2 += bench(sumToRecursion);
+}
+
+//alert('Итоговое время цикл: ' + time1);
+alert('Итоговое время рекурсия: ' + time2);
