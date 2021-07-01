4e682cc1bcd0c25f700845ece311f1e92c7f3f9f
Jhonah <buryakone@gmail.com>
2021-07-01 16:16:46 +0700

    рекурсивный обход, когда структура состоит из множества похожих вложенных объектов и массивов, например как Структура персонала

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
-function pow(x, n) {
-    if (n == 0) {
-        return 1;
+function sumSalaries(department) {
+    if (Array.isArray(department)) {
+        return department.reduce((previous, current) => previous + current.salary, 0);
     } else {
-        return pow(x, n - 1) * x;
+        let sum = 0;
+        for (let value of Object.values(department)) {
+            sum += sumSalaries(value);
+        }
+        return sum;
     }
 }
 
-alert( pow(2, 4) );
+let company = {
+    sales: [{
+        name: 'Peter',
+        salary: 1000
+    }, {
+        name: 'Alice',
+        salary: 600
+    }],
+
+    development: {
+        sites: [{
+            name: 'Jhon',
+            salary: 2000
+        }, {
+            name: 'Alex',
+            salary: 1800
+        }],
+
+        internals: [{
+            name: 'Jack',
+            salary: 1300
+        }]
+    }
+};
+
+alert( sumSalaries(company) );
