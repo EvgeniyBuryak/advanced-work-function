44b193ce2f92ffd3885e17a3abe810917f6ac647
Jhonah <buryakone@gmail.com>
2021-07-01 18:58:47 +0700

    тест бенчмаркинга для цикла и рекурсии

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ function sumToRecursion(num) {
     }
 }
 
+function sumTo(num) {
+    let sum = 0;
+    for (let i = 0; i < num; i++) {
+        sum += num - i;
+    }
+    return sum;
+}
+
 /*
 console.log(sumTo(1));  // = 1
 console.log(sumTo(2));  // = 2 + 1 = 3
@@ -19,23 +27,24 @@ function bench(f) {
     let date2 = new Date();
 
     let start = Date.now();
-    for (let i = 0; i < 100000; i++) f(100); // тестируем на 100
+    for (let i = 0; i < 1000; i++) //old value 100000
+        f(10000); // тестируем на old value 100
     return Date.now() - start;
 }
 
-//let time1 = 0;
+let time1 = 0;
 let time2 = 0;
 
 // добавляем для "разогрева" перед основным циклом
-//bench(sumTo);
+bench(sumTo);
 bench(sumToRecursion);
 
 // а теперь тестируем производительность
 // bench поочерёдно запускаются 10 раз
 for (let i = 0; i < 10; i++) {
-    //time1 += bench(sumTo);
+    time1 += bench(sumTo);
     time2 += bench(sumToRecursion);
 }
 
-//alert('Итоговое время цикл: ' + time1);
+alert('Итоговое время цикл: ' + time1);
 alert('Итоговое время рекурсия: ' + time2);
