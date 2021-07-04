8156db8b0b7bdc759a26ad1a66e4f7ccc6984349
Jhonah <buryakone@gmail.com>
2021-07-04 14:33:02 +0700

    fibonachi circle and recursion

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-function factorial(n) {
-    if (n <= 1) {
-        return 1;
-    } else {
-        return n * factorial(n - 1);
+// Рекурсивный способ, который не подвисает на числе 77, если больше не проверял
+let results = [0, 1];
+function fib(n) {
+    if (n == 0) return results[0];
+
+    if (n == 1) return results[1];
+
+    if (!results[n]) {
+        results[n] = fib(n - 2) + fib(n - 1);
     }
+    return results[n];
 }
 
-function factorialTernarni(n) {
-    return (n != 1) ? n * factorialTernarni(n - 1) : 1;
+function fibCircle(n) {
+    let a = 1;
+    let b = 1;
+    for (let i = 3; i <= n; i++) {
+        let c = a + b;
+        a = b;
+        b = c;
+    }
+    return b;
 }
 
-alert(factorial(5));
-alert(factorialTernarni(5));
+alert(fib(3)); // 2
+alert(fib(7)); // 13
+alert(fib(77)); // 5527939700884757
+alert(fibCircle(77));
