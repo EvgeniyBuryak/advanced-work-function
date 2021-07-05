f002820520967935c05515f3ab37e6ed993c460b
Jhonah <buryakone@gmail.com>
2021-07-05 16:01:53 +0700

    Вывод односвязного списка- варианты решения из задания через рекурсию и цикл

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 function printList(list) {    
 
-    if (list == null) return null;
+    alert(list.value);
 
-    let result = "";
-
-    for (let [key, value] of Object.entries(list)) {
-        
-        if (key != "value") {
-            return result + printList(value);
-        } else {
-            result = value + " ";
-        }
+    if (list.next) {
+        printList(list.next);
     }
 }
 
