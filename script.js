fda719976e695421901adf03e5018b6a2d18071e
Jhonah <buryakone@gmail.com>
2021-07-06 13:07:21 +0700

    Замыкание - Лексическое Окружение

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function showName(firstName, lastName, ...titles) {
-    alert(firstName + ' ' + lastName); // Arnold Washington
+function Counter() {
+    let count = 0;
 
-    // Rest arguments into in array
-    // titles = ["Consul", "Imperator"]
-    alert(titles[0]); // Consul
-    alert(titles[1]); // Imperator
-    alert(titles.length); // 2
+    this.up = function () {
+        return ++count;
+    };
+    this.down = function () {
+        return --count;
+    };
 }
 
-showName("Arnold", "Washington", "Consul", "Imperator");
+let counter = new Counter();
+let counter2 = new Counter();
 
-let arr = [3, 5, 7, 1];
-let arr2 = [6, 9, -2, 1];
-
-let merged = [12, ...arr, 13, ...arr2];
-
-alert(Math.max(...merged));
+alert(counter.up()); // ?
+alert(counter.up()); // ?
+alert(counter.down()); // ?
+alert(counter2.up()); // ?
