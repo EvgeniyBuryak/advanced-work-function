ed875803b0e2f22f2ce0ff8c81e7c8b86026b2a6
Jhonah <buryakone@gmail.com>
2021-07-07 22:41:15 +0700

    Сумма с произвольным количеством скобок без рекурсии

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-let sum = function func(a) {
+let sum = function (a) {
 
-    function counter(b) {
-        return func(a + b);
+    function f(b) {
+        f.currentSum += b; 
+        return f;
     };
 
-    counter.toString = () => {
-        return a;
+    f.currentSum = a;
+
+    f.toString = () => {
+        return f.currentSum;
     };
 
-    return counter;
+    return f;
 }
 
-console.log(sum(1)(2)); // == 3; // 1 + 2
-console.log(+sum(1)(2)(3)); // == 6; // 1 + 2 + 3
+alert(sum(1)(2)); // == 3; // 1 + 2
+console.log(sum(1)(2)(3)); // == 6; // 1 + 2 + 3
 console.log(sum(5)(-1)(2)); // == 6  
 console.log(sum(6)(-1)(-2)(-3)); // == 0
 console.log(sum(0)(1)(2)(3)(4)(5)); // == 15
