ddcd5e4a3855fe8cb866c57a88c0ef4520c22c79
Jhonah <buryakone@gmail.com>
2021-07-08 17:42:05 +0700

    Декоратор call - Обёртка для другой функции при нескольких аргументах

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-// do it worker.slow caching
 let worker = {
-    someMethod() {
-        return 1;
-    },
-
-    slow(x) {
-        // here super hard task for processor
-        alert("Called with " + x);
-        return x * this.someMethod(); // (*)
+    slow(min, max) {
+        alert(`Called with ${min},${max}`);
+        return min + max;
     }
 };
 
-function cachingDecorator(func) {
+function cachingDecorator(func, hash) {
     let cache = new Map();
-    return function (x) {
-        if (cache.has(x)) {
-            return cache.get(x);
+    return function () {
+        let key = hash(arguments); // (*)
+        if (cache.has(key)) {
+            return cache.get(key);
         }
-        let result = func.call(this, x); // (**)
-        cache.set(x, result);
+
+        let result = func.call(this, ...arguments); // (**)
+
+        cache.set(key, result);
         return result;
     };
 }
 
-alert(worker.slow(1)); // original method
+function hash(args) {
+    return args[0] + ',' + args[1];
+}
 
-worker.slow = cachingDecorator(worker.slow); // caching it
+worker.slow = cachingDecorator(worker.slow, hash);
 
-alert(worker.slow(2)); 
+alert(worker.slow(3, 5)); // работает
+alert("Again " + worker.slow(3, 5)); // аналогично (из кеша)
