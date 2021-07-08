4c0900e626fd1dc4f5656b951687adddb535d8be
Jhonah <buryakone@gmail.com>
2021-07-08 13:52:08 +0700

    Декораторы и переадресация вызова - Обертка функцией другую функцию

--- script.js
+++ script.js
@@ -1,38 +1,28 @@
-// outputs every second
-/*function printNumbers(from, to) {
-
-    let timerId = setInterval(() => {
-        (from <= to) ? alert("Число " + from++) : clearInterval(timerId)
-    }, 1000);
-}*/
-
-// The First call is instant    
-function printNumbers(from, to) {
-  let current = from;
-
-  function go() {
-    alert(current);
-    if (current == to) {
-      clearInterval(timerId);
-    }
-    current++;
-  }
-
-  go();
-  let timerId = setInterval(go, 1000);
+function slow(x) {
+    // здесь могут быть ресурсоёмкие вычисления
+    alert(`Called with ${x}`);
+    return x;
 }
 
-printNumbers(5, 10);
+function cachingDecorator(func) {
+    let cache = new Map();
 
-function printNumbersTimeout(from, to) {
+    return function (x) {
+        if (cache.has(x)) {    // если кеш содержит такой x,
+            return cache.get(x); // читаем из него результат
+        }
 
-    let timerId = setTimeout(function func() {
+        let result = func(x); // иначе, вызываем функцию
 
-        if (from <= to) {
-            alert("Число " + from++)
-            timerId = setTimeout(func(), 1000);
-        } else { clearInterval(timerId); }
-    }, 1000);
+        cache.set(x, result); // и кешируем (запоминаем) результат
+        return result;
+    };
 }
 
-printNumbersTimeout(1, 5);
+slow = cachingDecorator(slow);
+
+alert(slow(1)); // slow(1) кешируем
+alert("Again: " + slow(1)); // возвращаем из кеша
+
+alert(slow(2)); // slow(2) кешируем
+alert("Again: " + slow(2)); // возвращаем из кеша
