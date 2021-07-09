beffdb676fdf49ee5bc965a570dbc02bcec4f912
Jhonah <buryakone@gmail.com>
2021-07-09 14:26:13 +0700

    Задерживающий декоратор

--- script.js
+++ script.js
@@ -1,33 +1,23 @@
-function work(a, b) {
-    alert(a + b);
+let obj = {
+    f(x, y) {
+        alert(`${ x } ${ y }`);
+    },
+};
+
+
+function delay(func, ms) {
+
+    return function wrapper() {
+        setTimeout(() => func.apply(this, arguments), ms);
+        //setTimeout(func, ms, ...arguments); // так тоже работает
+    }
 }
 
-/**
- * Создайте декоратор spy(func), который должен
- * возвращать обёртку, которая сохраняет все вызовы
- * функции в своём свойстве calls.
- *
- * Каждый вызов должен сохраняться как массив аргументов.
- * */
-function spy(func) {
+// создаём обёртку 
+let f1000 = delay(obj.f, 1000);
+let f2500 = delay(obj.f, 2500);
+obj.f = delay(obj.f, 5000);
 
-    function wrapper(...args) {
-
-        wrapper.calls.push(args);
-
-        func.apply(this, args);
-    };
-
-    wrapper.calls = [];
-
-    return wrapper;
-}
-
-work = spy(work);
-
-work(1, 2);
-work(3, 4);
-
-for (let args of work.calls) {
-    alert('call:' + args.join());
-}
+f1000("Hello", "Igor");
+f2500("Hello", "Jhon");
+obj.f("my","test");
