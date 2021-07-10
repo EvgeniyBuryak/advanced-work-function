8cf03c085696e6e7847e7e525a1403c9d2e10d02
Jhonah <buryakone@gmail.com>
2021-07-10 15:08:57 +0700

    Декоратор debounce мой вариант - отклоняет повторные вызовы в течение короткого промежутка времени

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-let obj = {
-    f(x, y) {
-        alert(`${ x } ${ y }`);
-    },
-};
+function debounce(func, ms) {
+    let start;
 
+    return function () {
+        if (!start) {
+            start = Date.now();
+            setTimeout(() => func.apply(this, arguments), ms);
+        }
 
-function delay(func, ms) {
-
-    return function wrapper() {
-        setTimeout(() => func.apply(this, arguments), ms);
-        //setTimeout(func, ms, ...arguments); // так тоже работает
-    }
+        if (start + ms < Date.now()) {
+            start = Date.now();
+            setTimeout(() => func.apply(this, arguments), ms);
+        }        
+    };
 }
 
-// создаём обёртку 
-let f1000 = delay(obj.f, 1000);
-let f2500 = delay(obj.f, 2500);
-obj.f = delay(obj.f, 5000);
+let f = debounce(alert, 1000);
 
-f1000("Hello", "Igor");
-f2500("Hello", "Jhon");
-obj.f("my","test");
+f(1);
+f(2);
+
+setTimeout( () => f(3), 100);
+setTimeout( () => f(4), 1100);
+setTimeout( () => f(5), 1500);
